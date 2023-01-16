6d9411a9daf24e6862e4fa2c3dcd8de8edfe2b4b
tahmidbugs <tahmidahmed1@usf.edu>
2023-01-16 18:37:10 -0500

    testcodes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config();
     return result;
   }
   const name = random_name_generator();
-  const email = `${name}@examaple.com`;
+  const email = `${name}@examaple.co`;
   const emaillog = "habibi@gmail.com";
   const passwordlog = "1234567";
   const username = `${name}`;
